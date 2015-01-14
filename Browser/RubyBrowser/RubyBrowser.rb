12f805ea30ec9bacdd63260d4c90e6f0beb1f769
matthewarmand <marmand68@gmail.com>
2015-01-14 13:13:30 -0500

    Added client functionality to gui browser, skeleton service methods

--- Browser/RubyBrowser/RubyBrowser.rb
+++ Browser/RubyBrowser/RubyBrowser.rb
@@ -1,21 +1,35 @@
 require 'Shoes'
+require 'socket'
 
 Shoes.app(title: "RubyBrowser", width: 600, height: 400) do
-    stack margin: 10 do
+    stack margin: 10 do #double check Shoes docs, make sure we need stack
         flow do
             @address_bar = edit_line width: 500
             @go_button = button "GO"
             @go_button.click do
-                #process address_bar text here
-                url = @address_bar.text()
-                #Shoes has built in http requests, but we cant use those
-                #so here, create and submit request to server
-                #then load page (content) and parse markup
-                
+                @trigger_request(@address_bar.text())
             end
             #can put necessary UI elements here...
         end
         #or here, outside the flow...
     end
     #or here, outside the stack in a different stack/flow
+    def @trigger_request (url)
+        #split url into addr, port, filePath
+        socket = TCPSocket.new(addr, port)
+        socket.write("GET " + filePath + " HTTP/1.1\r\n\r\n")
+        while line = socket.gets
+            #maybe build into string, then pass to buildPage?
+        end
+    end
+
+    def @build_page (text)
+        #parse through text to create page content
+        #this may need to be moved...we'll play with it
+    end
+
+    def @on_link (url)
+        @address_bar.text(url)
+        trigger_request(url)
+    end
 end
